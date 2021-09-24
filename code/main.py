c650be22e581deb3fe89cc85c5596a9c48f45e59
Grigoriy Manucharian <90977525+old-zet@users.noreply.github.com>
2021-09-24 11:54:31 +0200

    main script

--- code/main.py
+++ code/main.py
@@ -1,53 +1,47 @@
-#! python3.8
+#!/usr/bin/env python
+# coding: utf-8
 
-from resolution_experimentale import *
-from operateurs import *
-from preconds_operateurs import *
+"""Solve Rubik's cube from a given initial state.
+"""
+
 import time
 
-'''etat_initial = [ # etat initial très difficile pour l'algo
-    ['O', 'O', 'G'],
-    ['B', 'O', 'R'],
-    ['R', 'Y', 'W'],
-    ['B', 'O', 'R', 'B', 'Y', 'W', 'O', 'G', 'W', 'B', 'R', 'Y'],
-    ['W', 'W', 'B', 'Y', 'B', 'W', 'B', 'Y', 'R', 'G', 'G', 'R'],
-    ['Y', 'G', 'Y', 'G', 'W', 'G', 'Y', 'W', 'W', 'R', 'B', 'O'],
-    ['B', 'Y', 'R'],
-    ['O', 'R', 'O'],
-    ['G', 'G', 'O']
-]'''
+from experimental_resolution import *
+from operators import *
+from operator_preconditions import *
+from classical_resolution import *
 
-# nombre de mouvements minimal absolu pour créer un mélange compliqué
-# je l'ai résolu en 43 mouvements, sans connaître les opérateurs appliqués
-etat_initial = copie_cube(etat_final)
-etat_initial = vrotate2(etat_initial)
-etat_initial = hrotate_inv2(etat_initial)
-etat_initial = plateau_rot_bas(etat_initial)
 
-print("État initial :")
-etat_en_texte(etat_initial)
-
-# interrupteur pour tests conditionnels plus simples à executer
-test_etoile = False
-
-start = time.time() # pour mésurer les tests
-
-if (test_etoile): # si test_etoile = True
-    chemin_final = []
-    print('avancement_sol_cube initial ', avancement_sol_cube(etat_initial)) # test 1
-    hybride(etat_initial, LIM, chemin_final)
+# 3 moves to mix it well enough. More moves means higher difficulty.
+initial_state = copy_cube(FINAL_STATE)
+initial_state = vrotate2(initial_state)
+initial_state = vrotate2(initial_state)
+initial_state = rotate_face_side_inv(initial_state)
+# Initial logging.
+print("Initial state:")
+state_to_text(initial_state)
+# A switch for hybrid method testing.
+star_test = False
+# For time measures.
+start = time.time()
+if star_test:  # Hybrid method.
+    final_path = []
+    # Test 1.
+    res = hybrid(initial_state, LIM, final_path)
+    print_solution(initial_state, res, state_to_text)
 else:
-    res = recherche_profondeur_limitee(etat_initial, est_final, operateurs_disponibles, 5) # test 2
-    #res = recherche_profondeur_mem(etat_initial, est_final, operateurs_disponibles, etat_en_texte, {}) # test 3
-    #res = recherche_largeur(etat_initial, est_final, operateurs_disponibles)  # test 4
-    #res = recherche_profondeur(etat_initial, est_final, operateurs_disponibles)  # test 5
-    #res = recherche_profondeur_iterative(etat_initial, est_final, operateurs_disponibles)  # test 6
-    #res = recherche_profondeur_random(etat_initial, est_final, operateurs_disponibles)  # test 7
-    affiche_solution(etat_initial, res, etat_en_texte) # affichage du cube
-
+    # Test 2.
+    # res = depth_limited_search(initial_state, is_final, available_operators, 5)
+    # Test 3.
+    # res = depth_first_memory_search(initial_state, is_final, available_operators, state_to_text, {})
+    # Test 4.
+    # res = breadth_first_search(initial_state, is_final, available_operators)
+    # Test 5.
+    # res = depth_first_search(initial_state, is_final, available_operators)
+    # Test 6.
+    res = depth_first_iterative_search(initial_state, is_final, available_operators)
+    # Test 7.
+    # res = depth_first_random_search(initial_state, is_final, available_operators)
+    print_solution(initial_state, res, state_to_text)
 end = time.time()
-print(end - start)
-
-#pos = get_case(etat_initial) # pour des tests de get_case
-#res = get_mult_path(etat_initial, operateurs_disponibles, LIM, pos) # pour des tests de get_mult_path
-#print(res)
+print(f'Time elapsed: {end-start}')
